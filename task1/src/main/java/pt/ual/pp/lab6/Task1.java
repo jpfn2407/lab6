c62e03484e829b246b887372413c80f375aee310
amgs <undisclosed>
2021-05-24 17:28:52 +0100

    Task1: done.

--- task1/src/main/java/pt/ual/pp/lab6/Task1.java
+++ task1/src/main/java/pt/ual/pp/lab6/Task1.java
@@ -1,9 +1,7 @@
 package pt.ual.pp.lab6;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -21,7 +19,7 @@ public class Task1 {
 //        return s.charAt(0) == 'A';
 //    }
 
-    public static List<String> filterStrings(List<String> strings, Predicate<String> predicate){
+    public static List<String> filterStrings(List<String> strings, Predicate<String> predicate) {
 //        List<String> result = new ArrayList<>();
 //        for(final var string : strings) {
 ////            if(filter(string)) {
@@ -42,13 +40,21 @@ public class Task1 {
                 "It was a really good Monday for being a Saturday.",
                 "The sudden rainstorm washed crocodiles into the ocean."
         );
-        System.out.println(strings);
+//        System.out.println(strings);
+//
+//        System.out.println("Filtered:");
+//        filterStrings(strings, s -> s.charAt(0) == 'A').forEach(System.out::println);
+//        filterStrings(strings, string -> string.length() > 10).forEach(System.out::println);
+//        filterStrings(strings, string -> string.split(" ").length > 8).forEach(System.out::println);
 
-        System.out.println("Filtered:");
-        List<String> filtered = filterStrings(strings,  s -> s.charAt(0) == 'A');
-        System.out.println(filtered);
-
-//        filterStrings(strings, string -> string.length() > 10);
-//        filterStrings(strings, string -> string.split(" ").length > 8);
+//        *Strings* com todas as palavras com 3 ou mais letras;
+//        filterStrings(strings,
+//                string -> filterStrings(Arrays.asList(string.split(" ")), s -> s.length() < 3).size() == 0)
+//                .forEach(System.out::println);
+//        *Strings* com um número par de letras `w` minúsculas.
+        filterStrings(strings, string ->
+                (string.length() - string.replace("w", "").length()) % 2 == 0 &&
+                (string.length() - string.replace("w", "").length()) != 0)
+                .forEach(System.out::println);
     }
 }
