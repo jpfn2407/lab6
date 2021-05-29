813647dcf260bb1bb0b88c00c667c3ddae91e2b1
João Ferreira <jpfn2407@gmail.com>
2021-05-29 15:46:38 +0100

    jn

---
+++ task1/src/main/java/pt/ual/pp/App.java
@@ -0,0 +1,22 @@
+package pt.ual.pp;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class App {
+
+    public static List<String> filterStrings(List<String> strings, Predicate<String> predicate){
+
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList(
+                "As the asteroid hurtled toward earth, Becky was upset her dentist appointment had been canceled.",
+                "Malls are great places to shop; I can find everything I need under one roof.",
+                "The blue parrot drove by the hitchhiking mongoose.",
+                "It was a really good Monday for being a Saturday.",
+                "The sudden rainstorm washed crocodiles into the ocean."
+        );
+
+    }
+}
---
+++ task2/src/main/java/pt/ual/pp/App.java
@@ -0,0 +1,14 @@
+package pt.ual.pp;
+
+
+class StudentRecord{
+
+}
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
